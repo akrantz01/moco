870c395c27f2146902a6432bff753662743eb0fa
Alex Krantz <alex@krantz.dev>
2023-07-25 10:43:22 -0700

    feat: wait for signal before exiting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use eyre::WrapErr;
+use tokio::signal;
 
 mod api;
 mod cli;
@@ -17,8 +18,42 @@ async fn main() -> eyre::Result<()> {
 
     let args = cli::parse();
 
-    let mut client = LemmyApi::connect(&args.api_url).await?;
-    client.login(&args.username, &args.password).await?;
+    let mut client = LemmyApi::connect(&args.api_url)
+        .await
+        .wrap_err("connection to instance failed")?;
+    client
+        .login(&args.username, &args.password)
+        .await
+        .wrap_err("login failed")?;
+
+    wait_for_terminate().await;
 
     Ok(())
+}
+
+async fn wait_for_terminate() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install ctrl+c handler")
+    };
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install terminate signal handler")
+            .recv()
+            .await
+    };
+    #[cfg(windows)]
+    let terminate = async {
+        signal::windows::ctrl_close()
+            .expect("failed to install close signal handler")
+            .recv()
+            .await
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
